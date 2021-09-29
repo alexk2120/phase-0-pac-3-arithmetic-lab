07100806914de5db10ebe7cb7bcb660fcc2edbf2
alexander.Khatamov <alexk2120@gmail.com>
2021-09-29 18:06:08 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
+
+  function subtract(x, y) {
+    return x - y;
+  }
+  console.log(subtract(1, 2));
+  
+  function multiply(x, y) {
+    return x * y;
+  }
+  console.log(multiply(1, 2));
+
+  function divide(x, y) {
+  return x / y;
+  }
+  console.log(divide(1, 2));
+
+
+  function increment(x) {
+    return x + 1;
+} 
+  
+function decrement(x) {
+    return x - 1;
+} 
+function parseInt(n){ 
+parseInt("x",Number) 
+}
